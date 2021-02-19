7cd039c800cdeb1ef83779818734c616e3a16b7a
LindholmGithub <christianlindholm@jubii.dk>
2021-02-19 13:13:02 +0100

    Added finishing touches!

---
+++ src/AttendanceChecker/GUI/Controllers/MoreInfoViewController.java
@@ -0,0 +1,50 @@
+package AttendanceChecker.GUI.Controllers;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MoreInfoViewController implements Initializable {
+    @FXML
+    public Label selectedStudentName;
+    @FXML
+    public PieChart absencePieChart;
+    @FXML
+    public Label selectedStudentMostAbsentDay;
+    @FXML
+    public Label selectedStudentTotalAbsenceDays;
+    @FXML
+    public Button closeMoreInfo;
+    @FXML
+    public Button loadChart;
+    @FXML
+    public Label selectedStudentPercentageAbsence;
+
+    public void handleCloseMoreInfo(ActionEvent actionEvent) {
+        Stage stage = (Stage) closeMoreInfo.getScene().getWindow();
+        stage.close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        selectedStudentName.setText(TeacherLoginViewController.getStudentInfoName());
+        ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                        new PieChart.Data("Absent", TeacherLoginViewController.getStudentInfoDaysAbsent()),
+                        new PieChart.Data("Present", (TeacherLoginViewController.getStudentInfoTotalDays()-TeacherLoginViewController.getStudentInfoDaysAbsent())));
+        absencePieChart.setData(pieChartData);
+        absencePieChart.setTitle("Absence chart:");
+        selectedStudentMostAbsentDay.setText(TeacherLoginViewController.getStudentInfoMostAbsentDay());
+        selectedStudentTotalAbsenceDays.setText(String.valueOf(TeacherLoginViewController.getStudentInfoDaysAbsent()));
+        selectedStudentPercentageAbsence.setText(TeacherLoginViewController.getStudentInfoPercentageAbsence()+"%");
+    }
+}
--- src/AttendanceChecker/GUI/Controllers/TeacherLoginViewController.java
+++ src/AttendanceChecker/GUI/Controllers/TeacherLoginViewController.java
@@ -2,21 +2,35 @@ package AttendanceChecker.GUI.Controllers;
 
 import AttendanceChecker.BLL.Be.Student;
 import AttendanceChecker.BLL.BllClass;
+import javafx.beans.property.ObjectProperty;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TeacherLoginViewController implements Initializable {
     private ObservableList<Student> studentObservableList;
     private BllClass bll;
+    private static Student selectedStudent;
+    private static String studentInfoName;
+    private static String studentInfoMostAbsentDay;
+    private static int studentInfoDaysAbsent;
+    private static int studentInfoTotalDays;
+    private static int studentInfoPercentageAbsence;
+
     @FXML
     public Button showInfo;
     @FXML
@@ -44,8 +58,48 @@ public class TeacherLoginViewController implements Initializable {
         studentDaysAbsentColumn.setCellValueFactory(cellData -> cellData.getValue().totalDaysProperty());
         studentList.setItems(studentObservableList);
     }
-    public void handleShowInfo(ActionEvent actionEvent) {
+    public void handleShowInfo(ActionEvent actionEvent) throws IOException {
+        selectedStudent = studentList.getSelectionModel().getSelectedItem();
+        studentInfoName = selectedStudent.getName();
+        studentInfoMostAbsentDay = selectedStudent.getMostAbsentDay();
+        studentInfoDaysAbsent = selectedStudent.getAbsentDays();
+        studentInfoTotalDays = selectedStudent.getTotalDays();
+        studentInfoPercentageAbsence = selectedStudent.getAbsentPercent();
+        if (selectedStudent != null) {
+            URL urlMoreInfo = new File("src/AttendanceChecker/GUI/Views/MoreInfoView.fxml").toURI().toURL();
+            Parent root = FXMLLoader.load(urlMoreInfo);
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setTitle("More Student Info");
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
 
+    public static Student getSelectedStudent() {
+        return selectedStudent;
+    }
+
+    public static String getStudentInfoName() {
+        return studentInfoName;
+    }
+
+    public static String getStudentInfoMostAbsentDay() {
+        return studentInfoMostAbsentDay;
+    }
+
+    public static int getStudentInfoDaysAbsent() {
+        return studentInfoDaysAbsent;
+    }
+
+    public static int getStudentInfoTotalDays() {
+        return studentInfoTotalDays;
+    }
+
+    public static int getStudentInfoPercentageAbsence() {
+        return studentInfoPercentageAbsence;
     }
 
     public void handleLogoutTeacher(ActionEvent actionEvent) {
