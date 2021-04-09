b537750e9d543661e875f14fe1e80e39ccb7f386
Mads Harby <71691732+madshby@users.noreply.github.com>
2021-04-09 09:22:23 +0200

    Updated Teacher Class with getters and setters

--- src/AttendanceChecker/Be/Teacher.java
+++ src/AttendanceChecker/Be/Teacher.java
@@ -8,4 +8,36 @@ public class Teacher {
     private String teachercode;
     private List<Course> courses;
     private List<Integer> courseId;
+
+
+    public Teacher(int id, String name, String teachercode){
+        this.id = id;
+        this.name = name;
+        this.teachercode = teachercode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTeachercode() {
+        return teachercode;
+    }
+
+    public void setTeachercode(String teachercode) {
+        this.teachercode = teachercode;
+    }
+    
 }
