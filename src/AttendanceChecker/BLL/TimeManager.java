4f05285b7aa4fb55b63e8ad77c3ffe9745d86d24
pete9991 <71692384+pete9991@users.noreply.github.com>
2021-04-09 11:45:53 +0200

    javadox and english
    added java dox to the 2 classes and changed some variabels to english

--- src/AttendanceChecker/BLL/TimeManager.java
+++ src/AttendanceChecker/BLL/TimeManager.java
@@ -6,20 +6,25 @@ import java.util.Timer;
 
 
 public class TimeManager {
-    private int time;
-    private int minut;
-    private int sekund;
+    private int hour;
+    private int minute;
+    private int second;
     public TimeManager() {
-        time = 16;
-        minut = 0;
-        sekund = 0;
+        hour = 16;
+        minute = 0;
+        second = 0;
     }
 
+    /**
+     * This method executes the method "TimeManagerTask.MyTimeTask" at the time designated by
+     * the variabels hour, minute and second.
+     * @throws ParseException
+     */
     public void schedualAtTimeTest() throws ParseException {
         Calendar goTime = Calendar.getInstance();
-        goTime.set(Calendar.HOUR_OF_DAY, time);
-        goTime.set(Calendar.MINUTE, minut);
-        goTime.set(Calendar.SECOND, sekund);
+        goTime.set(Calendar.HOUR_OF_DAY, hour);
+        goTime.set(Calendar.MINUTE, minute);
+        goTime.set(Calendar.SECOND, second);
         Timer timer = new Timer();
         timer.schedule(new TimeManagerTask.MyTimeTask(), goTime.getTime());
     }
--- src/AttendanceChecker/BLL/TimeManagerTask.java
+++ src/AttendanceChecker/BLL/TimeManagerTask.java
@@ -3,6 +3,10 @@ package AttendanceChecker.BLL;
 import java.util.TimerTask;
 
 public class TimeManagerTask {
+    /**
+     * This "class within a class" is structured this way, as it is the only way it is able to be
+     * used by the TimeManager class.
+     */
     public static class MyTimeTask extends TimerTask
     {
 
