984b57beb8dbbb932c4d0f2dcc651b454fa3c9e3
WackyGlow <70566908+WackyGlow@users.noreply.github.com>
2021-04-09 13:26:13 +0200

    updated LoginDao
    created a method which returns a students info if it matches with the gives Login & Password

--- src/AttendanceChecker/DAL/DAO/LoginDAO.java
+++ src/AttendanceChecker/DAL/DAO/LoginDAO.java
@@ -1,8 +1,15 @@
 package AttendanceChecker.DAL.DAO;
 
+import AttendanceChecker.Be.Student;
 import AttendanceChecker.DAL.DBConnection.JDBCConnectionPool;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginDAO {
 
@@ -11,5 +18,30 @@ public class LoginDAO {
     public LoginDAO() throws IOException {
         connectionPool = JDBCConnectionPool.getInstance();
     }
+
+
+    public Student getAllStudents(String login, String password){
+
+        try (Connection connection = connectionPool.checkOut()) {
+            String sql = "Select * from Student INNER JOIN StudentLogin ON Student.StudentID = StudentLogin.StudentID WHERE StudentLogin.StudentLogin = " + login +" AND StudentLogin.StudentPassword = " + password;
+            Statement statement = connection.createStatement();
+            if (statement.execute(sql)) {
+                ResultSet resultSet = statement.getResultSet();
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("StudentId");
+                    String name = resultSet.getString("StudentName");
+                    int absentDays = resultSet.getInt("AbsentDays");
+                    boolean presentToday = resultSet.getBoolean("PresentToday");
+                    Student student = new Student(id,name,absentDays,presentToday);
+                    return student;
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return null;
+    }
+
 
 }
